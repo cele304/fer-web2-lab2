4ee1f314c47f2aa1e9e5c6c0a7963c3a2fe19148
cele304 <129319909+cele304@users.noreply.github.com>
2024-10-28 13:57:25 +0100

    Update app.js

--- app.js
+++ app.js
@@ -25,6 +25,10 @@ const pool = new Pool({
     database: process.env.DB_DATABASE,
     password: process.env.DB_PASSWORD,
     port: process.env.DB_PORT,
+
+    ssl: {
+        rejectUnauthorized: false,
+    }, //dodano ovo za deploy
 });
 
 
